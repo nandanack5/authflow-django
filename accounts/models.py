b253aeeb303710b2e299e1a6880f3203e29dee08
Nandana Rajeev <nandhanarajeeev@gmail.com>
2026-06-18 13:13:51 +0530

    dashboard ui

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    profile_image = models.ImageField(
+        upload_to="profile_images/",
+        default="profile_images/default.png"
+    )
+
+    def __str__(self):
+
+        return self.user.username
 
 # Create your models here.
